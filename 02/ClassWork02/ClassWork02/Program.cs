7a94153c0eea6f27ef6b41476beafe0660d9aab8
gcuga <59294376+gcuga@users.noreply.github.com>
2019-12-29 14:13:06 +0300

    Class work of the 2st lesson added

---
+++ 02/ClassWork02/ClassWork02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassWork02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maximumTemperatureLevel = 39;
+            Console.WriteLine("maximumTemperatureLevel = " + maximumTemperatureLevel);
+
+            string myNumberAsText = "4.5";
+
+            float number = float.Parse(myNumberAsText, CultureInfo.InvariantCulture);
+
+            Console.WriteLine(-number);
+
+            for (int i = 1; i < 5; i++) {
+                Console.WriteLine(int.MaxValue + i);
+            }
+
+            string radius;
+            Console.WriteLine("Введите радиус:");
+            radius = Console.ReadLine().Replace(',', '.');
+
+            Console.WriteLine("Длина окружности = " +
+                    (double.Parse(radius, CultureInfo.InvariantCulture) * 2D * Math.PI) );
+
+            
+        }
+    }
+}

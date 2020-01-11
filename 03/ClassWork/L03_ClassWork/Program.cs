86926456da8a7a8ea3c1f21cb12ad88bf7d0c615
gcuga <59294376+gcuga@users.noreply.github.com>
2020-01-11 14:20:56 +0300

    Class work of the 3d lesson added

---
+++ 03/ClassWork/L03_ClassWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+using System.Globalization;
+using System.Threading;
+using System.Text;
+using L03_ClassWork.Properties;
+
+namespace L03_ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ //           object s = 10;
+ //           s = (int) s + 5;
+ //           WriteLine(s);
+
+            if (args != null && args.Length > 0)
+            {
+                Thread.CurrentThread.CurrentCulture
+                    = Thread.CurrentThread.CurrentUICulture
+                        = CultureInfo.GetCultureInfo(args[0]);
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            }
+
+            //Console.InputEncoding = Encoding.Unicode;
+            Console.InputEncoding = new UTF8Encoding(false);
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //Write("Введите вещественное число: ");
+            Write(Resources.NumberPrompt);
+            string input = ReadLine().Trim();
+            WriteLine("\nВведено: " + input);
+            double number = Double.Parse(input);
+
+            WriteLine(Resources.Result + number*number);
+
+        }
+    }
+}
+

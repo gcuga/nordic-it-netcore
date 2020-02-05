9bda998eaad1f79b64bee4c5c5e02b76e51b1797
gcuga <59294376+gcuga@users.noreply.github.com>
2020-02-05 22:17:18 +0300

    Class work of the 10th lesson added

---
+++ 10/ClassWork/L10CW/Pet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L10CW
+{
+    class Pet
+    {
+        public enum AnimalKind { Unknown, Mouse, Cat, Dog }
+        public enum SexType { Unknown, M, F }
+
+        private string _birthPlace;
+        public AnimalKind Kind;
+        public string Name;
+
+        private SexType _sex;
+        public SexType Sex
+        {
+            get { return _sex; }
+            set 
+            {
+                if (!Enum.IsDefined(typeof(SexType), value))
+                {
+                    throw new ArgumentException("Задан несуществующий пол");
+                }
+                _sex = value;
+            }
+        }
+        public int Age { get; set; }
+
+        public string Description
+        {
+            get
+            {
+                return $"{Name} is a {Kind} ({Sex}) of {Age} years old." +
+                    $" birth place: {_birthPlace}.";
+            }
+        }
+        public void SetBirthPlace(string birthPlace)
+        {
+            _birthPlace = birthPlace;
+        }
+    }
+}
---
+++ 10/ClassWork/L10CW/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+// https://planetcalc.ru/534/
+
+namespace L10CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet pet1 = new Pet();
+            pet1.Kind = Pet.AnimalKind.Dog;
+            pet1.Name = "Rex";
+            pet1.Sex = Pet.SexType.M;
+            pet1.Age = 5;
+            pet1.SetBirthPlace("Деревня гадюкино");
+            Console.WriteLine(pet1.Description);
+
+            Pet pet2 = new Pet();
+            pet2.Kind = Pet.AnimalKind.Cat;
+            pet2.Name = "Mursik";
+            pet2.Sex = Pet.SexType.Unknown;
+            pet2.Age = 100500;
+            pet2.SetBirthPlace("Hell");
+            Console.WriteLine(pet2.Description);
+        }
+    }
+}

9e264d2111c0519537a253b1c1a65ec7e8f6f2ee
gcuga <git@gcugreyarea.com>
2020-02-07 03:37:48 +0300

    Home work of the 10th lesson added

---
+++ 10/HomeWork/L10HW/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace L10HW
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public uint Age { get; set; }
+
+        public uint AgeInFourYears 
+        {
+            get
+            {
+                return Age + 4;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return $"Name: {Name}, age in 4 years: {AgeInFourYears}.";
+            }
+        }
+
+        public Person(string name, uint age)
+        {
+            Name = name; //?? throw new ArgumentNullException(nameof(name));
+            Age = age;
+        }
+    }
+}
---
+++ 10/HomeWork/L10HW/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace L10HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Person {i}");
+                Console.Write("Enter name: ");
+                string inputName;
+                do
+                {
+                    inputName = Console.ReadLine();
+                    if (inputName == null || inputName == string.Empty)
+                    {
+                        Console.Write("No name specified, please re-enter: ");
+                        continue;
+                    }
+
+                    break;
+                } while (true);
+
+                Console.Write("Enter age: ");
+                uint inputAge;
+                do
+                {
+                    if (!uint.TryParse(Console.ReadLine(), out inputAge))
+                    {
+                        Console.Write("Incorrect age, please re-enter: ");
+                        continue;
+                    }
+
+                    break;
+                } while (true);
+
+                persons[i] = new Person(inputName, inputAge);
+                Console.WriteLine();
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.Description);
+            }
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}

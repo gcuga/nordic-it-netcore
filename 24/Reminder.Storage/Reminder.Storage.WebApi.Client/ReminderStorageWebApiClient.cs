579e142638bf1cc0957e2a72af041ca9ad56cfc5
gcuga <git@gcugreyarea.com>
2020-04-04 00:27:30 +0300

    Home work of the 25th lesson added

---
+++ 24/Reminder.Storage/Reminder.Storage.WebApi.Client/ReminderStorageWebApiClient.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using Reminder.Storage.Core;
+using Reminder.Storage.WebApi.Core;
+
+namespace Reminder.Storage.WebApi.Client
+{
+	public class ReminderStorageWebApiClient : IReminderStorage
+	{
+		private HttpClient _httpClient;
+		private string _baseWebApiUrl;
+
+
+		public ReminderStorageWebApiClient(string baseWebApiUrl)
+		{
+			_httpClient = new HttpClient();
+			_baseWebApiUrl = baseWebApiUrl;
+		}
+
+		public int Count => throw new NotImplementedException();
+
+		public Guid Add(ReminderItemRestricted reminderItemRestricted)
+		{
+			HttpRequestMessage httpRequestMessage = new HttpRequestMessage(
+				HttpMethod.Post,
+				"https://localhost:44344/api/reminders/");
+
+			ReminderItemCreateModel reminderItemCreateModel =
+				new ReminderItemCreateModel(reminderItemRestricted);
+
+			string content = JsonConvert.SerializeObject(reminderItemCreateModel);
+			httpRequestMessage.Content = new StringContent(
+				content,
+				Encoding.UTF8,
+				"application/json");
+
+			// посылаем и получаем ответ
+			var httpResponceMessage = _httpClient.SendAsync(httpRequestMessage).Result;
+
+			if (httpResponceMessage.StatusCode != HttpStatusCode.Created)
+			{
+				throw new Exception(
+					$"Error: {httpResponceMessage.StatusCode}, " +
+					$"Content: {httpResponceMessage.Content.ReadAsStringAsync().Result}");
+			}
+
+			string resultContent = httpResponceMessage.Content.ReadAsStringAsync().Result;
+			ReminderItemGetModel reminderItemGetModel =
+				JsonConvert.DeserializeObject<ReminderItemGetModel>(resultContent);
+
+			return reminderItemGetModel.Id;
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ReminderItem Get(Guid id)
+		{
+			HttpRequestMessage httpRequestMessage = new HttpRequestMessage(
+				HttpMethod.Get,
+				"https://localhost:44344/api/reminders/" + id.ToString());
+
+			var httpResponseMessage = CallWebApi(HttpMethod.Get, id.ToString());
+				
+				_httpClient.SendAsync(httpRequestMessage).Result;
+
+			if (httpResponceMessage.StatusCode == HttpStatusCode.NotFound)
+			{
+				return null;
+			}
+
+			if (httpResponceMessage.StatusCode != HttpStatusCode.OK)
+			{
+				throw new Exception(
+					$"Error: {httpResponceMessage.StatusCode}, " +
+					$"Content: {httpResponceMessage.Content.ReadAsStringAsync().Result}");
+			}
+
+			string content = httpResponceMessage.Content.ReadAsStringAsync().Result;
+			ReminderItemGetModel reminderItemGetModel = 
+				JsonConvert.DeserializeObject<ReminderItemGetModel>(content);
+
+			return reminderItemGetModel.ToReminderItem();
+		}
+
+		public List<ReminderItem> Get(int count = 0, int startPostion = 0)
+		{
+			// TODO
+			return null;
+		}
+
+		public List<ReminderItem> Get(ReminderItemStatus status, int count = 0, int startPostion = 0)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Remove(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateStatus(IEnumerable<Guid> ids, ReminderItemStatus status)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateStatus(Guid id, ReminderItemStatus status)
+		{
+			throw new NotImplementedException();
+		}
+
+		private HttpResponseMessage CallWebApi(
+			HttpMethod method,
+			string relativeUrl,
+			string content = null)
+		{
+			if (content != null)
+			{
+				HttpRequestMessage httpRequestMessage = new HttpRequestMessage(
+					method,
+					_baseWebApiUrl + relativeUrl ?? string.Empty);
+			}
+
+
+					   
+			if (method == HttpMethod.Post || method == HttpMethod.Put)
+			{
+
+			}
+
+
+				"https://localhost:44344/api/reminders/");
+
+			ReminderItemCreateModel reminderItemCreateModel =
+				new ReminderItemCreateModel(reminderItemRestricted);
+
+			string content = JsonConvert.SerializeObject(reminderItemCreateModel);
+			httpRequestMessage.Content = new StringContent(
+				content,
+				Encoding.UTF8,
+				"application/json");
+
+			// посылаем и получаем ответ
+			var httpResponceMessage = _httpClient.SendAsync(httpRequestMessage).Result;
+
+			if (httpResponceMessage.StatusCode != HttpStatusCode.Created)
+			{
+				throw new Exception(
+					$"Error: {httpResponceMessage.StatusCode}, " +
+					$"Content: {httpResponceMessage.Content.ReadAsStringAsync().Result}");
+			}
+
+			string resultContent = httpResponceMessage.Content.ReadAsStringAsync().Result;
+			ReminderItemGetModel reminderItemGetModel =
+				JsonConvert.DeserializeObject<ReminderItemGetModel>(resultContent);
+		}
+	}
+}
---
+++ 25/Reminder.Storage/Reminder.Storage.WebApi/Controllers/RemindersController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Reminder.Storage.Core;
+using Reminder.Storage.WebApi.Core;
+
+namespace Reminder.Storage.WebApi.Controllers
+{
+	[ApiController]
+	[Route("api/reminders")]
+	public class RemindersController : ControllerBase
+	{
+		private readonly ILogger<RemindersController> _logger;
+		private readonly IReminderStorage _reminderStorage;
+
+		public RemindersController(
+			ILogger<RemindersController> logger,
+			IReminderStorage reminderStorage)
+		{
+			_logger = logger;
+			_reminderStorage = reminderStorage;
+		}
+
+		[HttpGet]
+		public IActionResult GetReminders(
+			[FromQuery(Name = "status")] string status = "",
+			[FromQuery(Name = "count")] int count = 0,
+			[FromQuery(Name = "startPostion")] int startPostion = 0)
+		{
+			ReminderItemStatus? reminderItemStatus = null;
+			if (!string.IsNullOrEmpty(status) && int.TryParse(status, out int parsedStatus))
+			{
+				if (Enum.IsDefined(typeof(ReminderItemStatus), parsedStatus))
+				{
+					reminderItemStatus = (ReminderItemStatus)parsedStatus;
+				}
+			}
+
+			List<ReminderItem> reminderItems;
+			if (reminderItemStatus != null)
+			{
+				reminderItems = 
+					_reminderStorage.Get((ReminderItemStatus) reminderItemStatus, count, startPostion);
+			}
+			else
+			{
+				reminderItems = _reminderStorage.Get(count, startPostion);
+			}
+
+			List<ReminderItemGetModel> reminderItemGetModels =
+				reminderItems
+					.Select(x => new ReminderItemGetModel(x))
+					.ToList();
+
+			return Ok(reminderItemGetModels);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetReminder(Guid id)
+		{
+			var reminderItem = _reminderStorage.Get(id);
+			if (reminderItem == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(new ReminderItemGetModel(reminderItem));
+		}
+
+		[HttpPost]
+		public IActionResult CreateReminder([FromBody] ReminderItemCreateModel reminderItemCreateModel)
+		{
+			if (reminderItemCreateModel == null)
+			{
+				return BadRequest();
+			}
+
+			var reminderItemRestricted = reminderItemCreateModel.ToReminderItemRestricted();
+			Guid id = _reminderStorage.Add(reminderItemRestricted);
+
+			var reminderItemGetModel = new ReminderItemGetModel(id, reminderItemRestricted);
+
+			return CreatedAtAction(
+				"GetReminder",
+				new { id },
+				reminderItemGetModel);
+		}
+
+		[HttpPatch("{id}")]
+		public IActionResult UpdateStatus(Guid id, [FromBody] ReminderItemStatus status)
+		{
+			var reminderItem = _reminderStorage.Get(id);
+			if (reminderItem == null)
+			{
+				return NotFound();
+			}
+
+			_reminderStorage.UpdateStatus(id, status);
+			reminderItem = _reminderStorage.Get(id);
+
+			return Ok(new ReminderItemGetModel(reminderItem));
+		}
+	}
+}
